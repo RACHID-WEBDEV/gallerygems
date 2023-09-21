917347cb52bd6abb5f7fb47bff22ec6f746dc056
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-09-21 21:03:13 +0100

    Modify gallery images

--- Data/gallery.tsx
+++ Data/gallery.tsx
@@ -8,165 +8,138 @@ const galleryData: galleryTypes[] = [
   {
     id: 1,
     tag: "africa man",
-    imageUrl:
-      "https://img.freepik.com/free-photo/medium-shot-smiley-man-carrying-food_23-2148761621.jpg?w=1800&t=st=1695154679~exp=1695155279~hmac=1d3d0b7a298ccb6776042a815d872ea6504dbc35f05d4fd7e043abc83cc02455",
+    imageUrl: "/gallery /man-food.avif",
   },
   {
     id: 2,
     tag: "nature",
-    imageUrl:
-      "https://img.freepik.com/free-photo/vertical-shot-some-beautiful-trees-sun-setting-background_181624-4522.jpg?w=826&t=st=1695156382~exp=1695156982~hmac=c503a70e6c08b5eb26d790ad1a0f2ab9ab144a5558b02e450be7354489d558ca",
+    imageUrl: "/gallery /beautiful-trees-sun.avif",
   },
   {
     id: 3,
     tag: "africa man",
-    imageUrl:
-      "https://img.freepik.com/free-photo/medium-shot-man-wearing-native-attire_23-2150397429.jpg?w=826&t=st=1695154584~exp=1695155184~hmac=19f914275d9924882270b83791bbf1e637b0c40275d8b0a738f57fc45d098d58",
+    imageUrl: "/gallery /africa-man.avif",
   },
   {
     id: 4,
     tag: "woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/portrait-young-female-jumping_23-2148885455.jpg?w=826&t=st=1695154102~exp=1695154702~hmac=b5555fa2769f5d39151a4884f91f3178e302e0e65dd26d0360a90a5c7a24c736",
+    imageUrl: "/gallery /portrait-young-female.avif",
   },
   {
     id: 5,
     tag: "woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/close-up-beautiful-young-woman-carnival-stylish-masquerade-costume-with-feathers-gradient-wall-neon-light-concept-holidays-celebration-festive-time-dance-party-having-fun_155003-28066.jpg?t=st=1695145706~exp=1695146306~hmac=6b3c1d0b5fc3e8bae118472ccec74d6d57dda66eae744100ac282e88ef6eb083",
+    imageUrl: "/gallery /beautiful-young-woman.avif",
   },
   {
     id: 6,
     tag: "art",
-    imageUrl:
-      "https://img.freepik.com/free-photo/celebrate-beautiful-young-women-carnival-stylish-masquerade-costume-with-feathers-black-background-neon-light-copyspace-ad-holidays-celebration-dancing-fashion-festive-time-party_155003-37259.jpg?t=st=1695145706~exp=1695146306~hmac=a3013d7db9cc003b94e2524281684fb59e83b216bd1f036197716c45bfffdf86",
+    imageUrl: "/gallery /celebrate-beautiful.avif",
   },
   {
     id: 7,
     tag: "woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/medium-shot-african-woman-dancing-alone_23-2148747974.jpg?w=1060&t=st=1695154183~exp=1695154783~hmac=154ecc200107f8a1479eb010e3ba4070cb029ae088287c426944567f60e70f2c",
+    imageUrl: "/gallery /african-woman-dancing-alone.avif",
   },
   {
     id: 8,
     tag: "nature",
-    imageUrl:
-      "https://img.freepik.com/free-photo/aerial-shot-amazing-mountain-landscape-apuseni-mountains-transylvania-romania_181624-42326.jpg?w=996&t=st=1695156430~exp=1695157030~hmac=663027c0de4c6e2bb36ed9050d8f43e373dd285c4cb32cda3aba888e4d5fe654",
+    imageUrl: "/gallery /mountain-landscape.avif",
   },
   {
     id: 9,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/animal-lizard-nature-multi-colored-close-up-generative-ai_188544-9072.jpg?w=2000&t=st=1695157028~exp=1695157628~hmac=1dfc037ce9a1b0c4a87a65a92f7d21d43489d618fe7e3ed529e20b299b4e2b07",
+    imageUrl: "/gallery /animal-lizard-nature.avif",
   },
   {
     id: 10,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/amazing-bengal-tiger-nature_475641-1295.jpg?w=826&t=st=1695157117~exp=1695157717~hmac=48712099f944b09d3c545c24d9f94eea165e3710efa3ec0a6877aeacf27e5f9c",
+    imageUrl: "/gallery /amazing-bengal-tiger.avif",
   },
   {
     id: 11,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?w=826&t=st=1695157135~exp=1695157735~hmac=daef235b893bca651aa40fa5649051285846a184a9d44d66db9e53bdc26ab7e8",
+    imageUrl: "/gallery /closeup-scarlet.avif",
   },
 
   {
     id: 12,
     tag: "nature",
-    imageUrl:
-      "https://img.freepik.com/free-photo/closeup-shot-leaf-beetle_181624-34840.jpg?w=1480&t=st=1695156228~exp=1695156828~hmac=f778d2eab846652f59c12599c43cb6986a6641b95e29eb699178f00fb0d4bbab",
+    imageUrl: "/gallery /leaf-beetle.avif",
   },
   {
     id: 13,
     tag: "nature",
-    imageUrl:
-      "https://img.freepik.com/free-photo/beautiful-view-greenery-bridge-forest-perfect-background_181624-17827.jpg?w=1800&t=st=1695156269~exp=1695156869~hmac=b251d878040a6a4de0031023615c7380eb71e9c6d017037c059e29476985e388",
+    imageUrl: "/gallery /beautiful-view.avif",
   },
   {
     id: 14,
     tag: "woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/upbeat-woman-chills-indoor-keeps-arms-raised-clenches-fists-dances-carefree_273609-40991.jpg?w=1800&t=st=1695154270~exp=1695154870~hmac=8ade10c7a5cbdd79927b55242fc955980ca36c4005d8d386512ecd99b919c13f",
+    imageUrl: "/gallery /upbeat-woman1.avif",
   },
   {
     id: 15,
     tag: "africa woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/portrait-african-woman-wearing-traditional-accessories-smiling_23-2148747960.jpg?w=826&t=st=1695154414~exp=1695155014~hmac=16458c4ff94ebe0626fc733930e2b4247f30a7fb59f3ba7bc0fa6c561501256c",
+    imageUrl: "/gallery /portrait-african-woman.avif",
   },
   {
     id: 16,
     tag: "africa woman",
-    imageUrl:
-      "https://img.freepik.com/free-photo/beautiful-african-women-having-fun-while-fetching-water_23-2149021892.jpg?w=1800&t=st=1695154469~exp=1695155069~hmac=a814049e51242860fffd4e7669f607a39ae90f679b2d30e014a13c31845dbef7",
+    imageUrl: "/gallery /beautiful-african-womenq.avif",
   },
   {
     id: 17,
     tag: "africa man",
-    imageUrl:
-      "https://img.freepik.com/free-photo/portrait-african-senior-man-with-traditional-clothes_23-2148833076.jpg?w=826&t=st=1695154770~exp=1695155370~hmac=e7a1791ed165daf2bd53812c08b8322b7c5d7d59efa5cbae885049d638942f74",
+    imageUrl: "/gallery /portrait-african-senior-man.avif",
   },
   {
     id: 18,
     tag: "man",
-    imageUrl:
-      "https://img.freepik.com/free-photo/medium-shot-male-flight-attendant-posing_23-2150312703.jpg?w=826&t=st=1695154804~exp=1695155404~hmac=eda62d5e0a15d73cc650bba2323c6b8660c9bc491509c770de2e2d6eec1fba74",
+    imageUrl: "/gallery /medium-shot-male.avif",
   },
   {
     id: 19,
     tag: "3d characters",
-    imageUrl:
-      "https://img.freepik.com/free-photo/fashion-little-boy_71767-95.jpg?w=1060&t=st=1695155037~exp=1695155637~hmac=6f60343d4e94d5d47513e8f93afdc0878f614486ae727349ceec7780fe1b20f6",
+    imageUrl: "/gallery /fashion-little-boy.avif",
   },
   {
     id: 20,
     tag: "3d characters",
-    imageUrl:
-      "https://img.freepik.com/free-psd/young-bearded-man-wearing-headphones-listening-music_1150-51123.jpg?w=1480&t=st=1695155143~exp=1695155743~hmac=5efdc27e12e78d9fcae72755e7f939e6f44bc8b8b83c09155b6cc1f5c0dacfcc",
+    imageUrl: "/gallery /man-wearing-headphone.avif",
   },
   {
     id: 21,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/impala-savannah_167946-146.jpg?w=826&t=st=1695157160~exp=1695157760~hmac=3409f5730653a3f7c27c841701e2671377839736f41c4448a7620738040840f7",
+    imageUrl: "/gallery /impala-savannah.avif",
   },
   {
     id: 22,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/african-leopard-female-pose-beautiful-evening-light_475641-34.jpg?w=1800&t=st=1695157224~exp=1695157824~hmac=026d08b36c8a565cf621d4754ae5a1ef79cfeaa4c321bb5b2d3032e12d654455",
+    imageUrl: "/gallery /african-leopard.avif",
   },
   {
     id: 23,
     tag: "animal",
-    imageUrl:
-      "https://img.freepik.com/free-photo/colorful-parrot-with-black-beak-yellow-green-feathers_1340-28847.jpg?t=st=1695157184~exp=1695160784~hmac=cf37f6b9b585dd5146cb5f3d117c438daa89286a846563ac18ae0837e70fe2d9&w=900",
+    imageUrl: "/gallery /colorful-parrot.avif",
   },
   {
     id: 24,
     tag: "3d characters",
-    imageUrl:
-      "https://img.freepik.com/free-photo/fun-3d-cartoon-teenage-boy_183364-81177.jpg?w=900&t=st=1695155189~exp=1695155789~hmac=20496fa2313ab6c43904caa45b95c050da78b97a39a1fad5876e4bb300197b67",
+    imageUrl: "/gallery /fun-3d-cartoon-teenage-boy.avif",
   },
   {
     id: 25,
     tag: "3d characters",
-    imageUrl:
-      "https://img.freepik.com/free-photo/cartoon-character-with-pan-pancake-her-head_1340-29120.jpg?t=st=1695155248~exp=1695158848~hmac=976c20a05afeed628714fd994f951772899f49429e93a11aa841e1b243bcf121&w=826",
+    imageUrl: "/gallery /cartoon-character-.avif",
   },
   {
     id: 26,
     tag: "3d characters",
-    imageUrl:
-      "https://img.freepik.com/free-psd/3d-illustration-cartoon-character-beard-businessman-standing-hold-tablet-his-arm-with-smile-rendering_1150-65934.jpg?w=2000&t=st=1695155341~exp=1695155941~hmac=d88ff066b3665f4b1255b4c77b735a49901952f88f4974be9d6e7ce6e3e92220",
+    imageUrl: "/gallery /3d-illustration.avif",
   },
   {
     id: 27,
     tag: "nature",
-    imageUrl:
-      "https://img.freepik.com/free-photo/closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower_181624-7640.jpg?w=1800&t=st=1695156152~exp=1695156752~hmac=7d414788ad02dda54a433235c43e5bf3d7d82d70f9193d274704846e85fc6c4f",
+    imageUrl: "/gallery /beautiful-butterfly.avif",
   },
 ];
 
