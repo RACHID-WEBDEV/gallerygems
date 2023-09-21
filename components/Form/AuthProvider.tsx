6b886467617199ca42cc25427a810360011b5a65
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-09-21 18:57:18 +0100

    gallery gems hngx

--- components/Form/AuthProvider.tsx
+++ components/Form/AuthProvider.tsx
@@ -3,11 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import React, { ReactNode } from "react";
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  return (
-    <SessionProvider basePath="/custom-route/api/auth">
-      {children}
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
